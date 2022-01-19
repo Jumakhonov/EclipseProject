54a30a6055cdc5253479c6c52c320966a8aa3bdd
jumakhonov <aleemjumakhonov@yahoo.com>
2022-01-18 21:40:38 -0800

    second commit

--- src/git/HelloWorld.java
+++ src/git/HelloWorld.java
@@ -3,8 +3,11 @@ package git;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		String name = "Hello World!";
+		System.out.println(name);
+		
+		
 	}
 
 }
